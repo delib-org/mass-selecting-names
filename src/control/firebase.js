51fe7f0b183c66a09c0570a9a679d226459df345
Tal Yaron <tal.yaron@gmail.com>
2019-12-07 18:24:34 +0200

    functions init

--- src/control/firebase.js
+++ src/control/firebase.js
@@ -2,15 +2,15 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const config = {
-    apiKey: "AIzaSyBEumZUTCL3Jc9pt7_CjiSVTxmz9aMqSvo",
-    authDomain: "synthesistalyaron.firebaseapp.com",
-    databaseURL: "https://synthesistalyaron.firebaseio.com",
-    projectId: "synthesistalyaron",
-    storageBucket: "synthesistalyaron.appspot.com",
-    messagingSenderId: "799655218679",
-    appId: "1:799655218679:web:d0118a27fd956d12"
-
-}
+	apiKey: 'AIzaSyD82XZxw9S90yrFl0PB5ibxy9MZv1GncH4',
+	authDomain: 'mass-names.firebaseapp.com',
+	databaseURL: 'https://mass-names.firebaseio.com',
+	projectId: 'mass-names',
+	storageBucket: 'mass-names.appspot.com',
+	messagingSenderId: '990355098057',
+	appId: '1:990355098057:web:af50ce9fd2aaf0d2e10018',
+	measurementId: 'G-GW6NL7QXD5'
+};
 
 firebase.initializeApp(config);
 
